d6c143a9a5efc4caf46798ef690b5e1d5f5c75ba
chhaya <chhaya@LAPTOP-QTOJQVJQ>
2017-10-08 19:01:00 +0530

    Commit

--- src/packageHive/MyUDF.java
+++ src/packageHive/MyUDF.java
@@ -17,8 +17,8 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 //class declaration which inherits the HIVE UDF class
 /**
- * This class will create a custome HIVE UDF where evaluate method of UDF class is invoked for each record of HIVE UDF.
- * This program accepts two arguments and concatenates the array elements of field value alongwith separator which is passed as a first argument.
+ * This class will create a custom HIVE UDF where evaluate method of UDF class is invoked for each record of HIVE UDF.
+ * This program accepts two arguments and concatenates the array elements of field value along with separator which is passed as a first argument.
  * 
  * Dated : 08-OCT-2016.
  * 
